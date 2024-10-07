8f5febabc03d4f9a5e5da72a6ba2b64a2b06f77a
dotdevdotdev <dotdevdotdev@gmail.com>
2024-10-06 22:44:01 -0400

    first commit

---
+++ src/main/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "../preload/preload.js"),
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "../../public/index.html"));
+
+  // Open DevTools in development mode
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ src/renderer/renderer.js
@@ -0,0 +1,4 @@
+// This file will be loaded by index.html
+console.log("Renderer process started");
+
+// We'll add more code here as we develop the application
